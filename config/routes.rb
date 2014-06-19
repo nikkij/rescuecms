25c6536a4402766c191b9be7d077a02982a4b4f4
nikkij <nikki.jacobson@foxleapmoon.com>
2014-06-19 04:43:01 +0000

    Added basic frontend home controller and admin dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root to: "home#index"
+
+  namespace :admin do
+    root to: "dashboard#index"
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
