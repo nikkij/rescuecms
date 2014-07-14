6e714218ccf9f4173d3abb9862fe7791e1865678
nikkij <nikki.jacobson@foxleapmoon.com>
2014-07-14 04:49:11 +0000

    Refining front end with boilerplate and demo copy.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,5 @@
 class HomeController < ApplicationController
+  def index
+    @animals = Animal.all
+  end
 end
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -15,6 +15,10 @@ class PictureUploader < CarrierWave::Uploader::Base
     process :resize_to_fill => [200, 300, :north]
   end
 
+  version :gallery do
+    process :resize_to_fill => [700, 450]
+  end  
+
   version :card do
     process :resize_to_fit => [210, 210]
   end
