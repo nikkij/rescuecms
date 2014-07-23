b00d2b66497e0bbda7aeacf05926b85274bb887b
nikkij <nikki.jacobson@foxleapmoon.com>
2014-07-23 09:13:48 +0000

    Cleaning up location test and adding more animal seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -506,7 +506,12 @@ on_premises_locations = OnPremisesLocation.create([
 	{area:'Cat Quarentine',unit:'2'}])
 
 
+
 animals = Animal.create([
 	{name:'Jasper',location_id:1},
 	{name:'Guinness',location_id:1},
-	{name:'Gizmo',location_id:2}])
+	{name:'Gizmo',location_id:2,remote_picture_url:'http://res.cloudinary.com/hwtphffjr/image/upload/t_media_lib_thumb/v1403932823/dwg9kpfduoltqcm1ni3w.jpg'},
+	{name:'Jesse',location_id:15,remote_picture_url:'http://res.cloudinary.com/hwtphffjr/image/upload/t_media_lib_thumb/v1406096720/cp8wdzfoluodl087dg9a.jpg'},
+	{name:'Pip',location_id:3,remote_picture_url:'http://res.cloudinary.com/hwtphffjr/image/upload/t_media_lib_thumb/v1403918766/nybuvyrogb64kaaffeuh.jpg'},
+    {name:'Dakota',location_id:4,remote_picture_url:'http://res.cloudinary.com/hwtphffjr/image/upload/c_thumb,g_face,h_100,w_150/v1406106211/bosjx0zaqytt9w6kd0mq.jpg'}
+])
--- spec/factories/animals.rb
+++ spec/factories/animals.rb
@@ -2,6 +2,5 @@ FactoryGirl.define do
   factory :animal do |f| 
   	f.name "Charlie"
   	f.picture {Rack::Test::UploadedFile.new(File.join(Rails.root,'spec','support','animal','pictures','pip.jpg'))}
-    #f.picture File.open(File.join(Rails.root,'spec','support','animal','pictures','pip.jpg'))
-  end 
+  end
 end
