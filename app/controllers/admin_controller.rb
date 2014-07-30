bae7a6a83fbbecc6e36f4cc15a0d1ea2b37c9024
nikkij <nikki.jacobson@foxleapmoon.com>
2014-07-30 11:54:42 +0000

    Add animal count to admin sidebar nav

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,11 @@
 class AdminController < ApplicationController
-  
+  before_filter :count_animals
+
   protected
+  
+  def count_animals
+  	@animal_count = Animal.all.count
+  end
 
   def user_for_paper_trail
     'Unknown user'
